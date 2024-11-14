d0c852d646bed773b5569cd26e4192892ae20f7d
Brian Song <brian.cs.song@gmail.com>
2024-11-14 03:52:18 +0000

    Adds a delete link for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,8 @@
 class MoviesController < ApplicationController
   def create
-    # Param has looks like tihs:
+    # params hash looks like this:
     # {"the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6"}
-
+    
     m = Movie.new
     m.title = params.fetch("the_title")
     m.year = params.fetch("the_year")
@@ -10,17 +10,29 @@ class MoviesController < ApplicationController
     m.description = params.fetch("the_description")
     m.image = params.fetch("the_image")
     m.director_id = params.fetch("the_director_id")
-
+    
     m.save
-
+    
     redirect_to("/movies")
-
-    # Retriev user's inputs from params
+    
+    # Retrieve the user's inputs from params
     # Create a record in the movie table
     # Populate each column with the user input
     # Save
-
+    
     # Redirect the user back to the /movies URL
+  end
+
+  def destory
+    the_id = params.fetch("an_id")
+
+    matching_records = Movie.where({ :id => the_id})
+
+    the_movie = matching_recods.at(0)
+
+    the_movie.destroy
+
+    redirect_to("/movies")
   end
   
   
